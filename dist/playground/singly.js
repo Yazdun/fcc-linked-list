bdfda477db9ea19b0716f4f8fd1834507a15a206
Yaz <yazduntube@gmail.com>
2025-05-23 13:41:33 +0330

    update playground component

--- dist/playground/singly.js
+++ dist/playground/singly.js
@@ -3,12 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.SinglyLinkedList = void 0;
 /** Node for singly linked list */
 class N {
-    /** Creates a node with given value */
-    constructor(value) {
-        /** Next node reference */
-        this.next = null;
-        this.data = value;
-    }
 }
 /** Singly linked list implementation */
 class SinglyLinkedList {
@@ -20,142 +14,33 @@ class SinglyLinkedList {
     // │ CORE OPERATIONS
     // └──────────────────────────┘
     /** Adds node to list start */
-    prepend(val) {
-        const newNode = new N(val);
-        newNode.next = this.head;
-        this.head = newNode;
-    }
+    prepend(val) { }
     /** Adds node to list end */
-    append(val) {
-        const newNode = new N(val);
-        if (!this.head) {
-            this.head = newNode;
-            return;
-        }
-        let current = this.head;
-        while (current.next) {
-            current = current.next;
-        }
-        current.next = newNode;
-    }
+    append(val) { }
     /** Removes head node */
-    deleteHead() {
-        if (this.head) {
-            this.head = this.head.next;
-        }
-    }
+    deleteHead() { }
     /** Removes tail node */
-    deleteTail() {
-        if (!this.head)
-            return;
-        if (!this.head.next) {
-            this.head = null;
-            return;
-        }
-        let current = this.head;
-        while (current.next && current.next.next) {
-            current = current.next;
-        }
-        current.next = null;
-    }
+    deleteTail() { }
     /** Removes first node with given value */
-    delete(val) {
-        if (!this.head)
-            return;
-        if (this.head.data === val) {
-            this.head = this.head.next;
-            return;
-        }
-        let current = this.head;
-        while (current.next) {
-            if (current.next.data === val) {
-                current.next = current.next.next;
-                return;
-            }
-            current = current.next;
-        }
-    }
+    delete(val) { }
     /** Finds node with given value */
     find(val) {
-        if (!this.head)
-            return null;
-        let current = this.head;
-        while (current) {
-            if (current.data === val)
-                return current;
-            current = current.next;
-        }
         return null;
     }
     /** Logs all node values */
-    traverse() {
-        let current = this.head;
-        while (current) {
-            console.log(current.data);
-            current = current.next;
-        }
-    }
+    traverse() { }
     // ┌────────────────────────────┐
     // │ BONUS OPERATIONS
     // └────────────────────────────┘
     /** Inserts node at given position */
-    insertAt(pos, val) {
-        const newNode = new N(val);
-        let current = this.head;
-        if (pos < 0)
-            throw new Error("failed");
-        if (pos === 0) {
-            newNode.next = this.head;
-            this.head = newNode;
-            return;
-        }
-        let idx = 0;
-        while (current && idx < pos - 1) {
-            current = current.next;
-            idx++;
-        }
-        if (!current)
-            throw new Error("failed");
-        newNode.next = current.next;
-        current.next = newNode;
-    }
+    insertAt(pos, val) { }
     /** Removes node at given position */
-    deleteAt(pos) {
-        let current = this.head;
-        let idx = 0;
-        if (pos === 0) {
-            this.head = current?.next ?? null;
-        }
-        while (current && idx < pos - 1) {
-            current = current.next;
-            idx++;
-        }
-        if (current && current.next) {
-            current.next = current.next.next;
-        }
-    }
+    deleteAt(pos) { }
     /** Finds middle node */
     findMiddle() {
-        let slow = this.head;
-        let fast = this.head;
-        while (fast && fast.next?.next) {
-            slow = slow?.next ?? null;
-            fast = fast.next.next;
-        }
-        return slow;
+        return null;
     }
     /** Reverses list in place */
-    reverse() {
-        let current = this.head;
-        let following = this.head;
-        let prev = null;
-        while (current) {
-            following = following?.next;
-            current.next = prev;
-            prev = current;
-            current = following;
-        }
-        this.head = prev;
-    }
+    reverse() { }
 }
 exports.SinglyLinkedList = SinglyLinkedList;
